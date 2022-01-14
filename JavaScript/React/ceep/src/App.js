f4d2940f00bda5647545584a35fff9db095c5ab5
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2022-01-14 15:26:12 -0800

    estudando

--- JavaScript/React/ceep/src/App.js
+++ JavaScript/React/ceep/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { FormularioCadastro } from './components/FormularioCadastro';
+import React, { Component } from 'react';
+import { FormularioCadastro } from './components/FormularioCadastro/FormularioCadastro';
 import { ListaDeNotas } from './components/ListaDeNotas/ListaDeNotas';
-function App() {
-  return (
-    <section>
+import "./assets/App.css";
+import './assets/index.css'
 
-      <FormularioCadastro/>
-      <ListaDeNotas/>
+class App extends Component {
 
-    </section>
-  );
+  criarNota(titulo, texto){
+    console.log(titulo + texto)
+  } 
+
+  render(){ 
+    return (
+      <section className="conteudo">
+
+        <FormularioCadastro criarNota={this.criarNota}/>
+        <ListaDeNotas/>
+
+      </section>
+    );
+  }
 }
 
 export default App;
