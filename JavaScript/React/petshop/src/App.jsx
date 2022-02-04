1c2918b3c29215bd96cfa679b3af2590451a79f1
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2022-02-03 17:38:41 -0800

    estudando

--- JavaScript/React/petshop/src/App.jsx
+++ JavaScript/React/petshop/src/App.jsx
@@ -6,6 +6,8 @@ import Cabecalho from "./components/cabecalho";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Pagina404 from "./paginas/400";
+import Post from "./paginas/post";
+import Categoria from "./paginas/Categoria";
 
 function App() {
   return (
@@ -18,6 +20,12 @@ function App() {
           </Route>
           <Route path={"/sobre"}>
             <Sobre />
+          </Route>
+          <Route path={"/categoria/:id"}>
+            <Categoria />
+          </Route>
+          <Route path={"/posts/:id"}>
+            <Post />
           </Route>
           <Route>
             <Pagina404 />
