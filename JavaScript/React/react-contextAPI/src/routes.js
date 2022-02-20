809523cc7de892b1db0053d201cc5f2aaaf5edd8
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2022-02-19 17:56:49 -0800

    estudando

---
+++ JavaScript/React/react-contextAPI/src/routes.js
@@ -0,0 +1,28 @@
+import Carrinho from "pages/Carrinho";
+import Feira from "pages/Feira";
+import Login from "pages/Login";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { UsuarioProvider } from 'commom/context/Usuario'
+
+function Router() {
+
+    return (
+        <BrowserRouter>
+            <Switch>
+                <UsuarioProvider>
+                    <Route exact path="/">
+                        <Login />
+                    </Route>
+                    <Route path="/feira">
+                        <Feira />
+                    </Route>
+                </UsuarioProvider>
+                <Route path="/carrinho">
+                    <Carrinho />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
