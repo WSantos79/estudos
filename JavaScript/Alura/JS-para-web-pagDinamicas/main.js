73f3b73239816d7bbc19044989c5f614ab5f208f
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2022-05-03 21:17:21 -0300

    estudando

---
+++ JavaScript/Alura/JS-para-web-pagDinamicas/main.js
@@ -0,0 +1,56 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+
+
+
+/*const pom = document.querySelector('.tecla_pom');
+const clap = document.querySelector('.tecla_clap');
+const tim = document.querySelector('.tecla_tim');
+
+const puff = document.querySelector('.tecla_puff');
+const splash = document.querySelector('.tecla_splash');
+const toim = document.querySelector('.tecla_toim');
+
+const psh = document.querySelector('.tecla_psh');
+const tic = document.querySelector('.tecla_tic');
+const tom = document.querySelector('.tecla_tom');
+
+pom.onclick = () => {
+    document.getElementById('som_tecla_pom').play();
+};
+
+clap.addEventListener('click', () => {
+    document.getElementById('som_tecla_clap').play();
+});
+
+tim.addEventListener('click', () => {
+    document.getElementById('som_tecla_tim').play();
+});
+
+puff.addEventListener('click', () => {
+    document.getElementById('som_tecla_puff').play();
+});
+
+splash.addEventListener('click', () => {
+    document.getElementById('som_tecla_splash').play();
+});
+
+toim.addEventListener('click', () => {
+    document.getElementById('som_tecla_toim').play();
+});
+
+psh.addEventListener('click', () => {
+    document.getElementById('som_tecla_psh').play();
+});
+
+tic.addEventListener('click', () => {
+    document.getElementById('som_tecla_tic').play();
+});
+
+tom.addEventListener('click', () => {
+    document.getElementById('som_tecla_tom').play();
+});
+
+*/
