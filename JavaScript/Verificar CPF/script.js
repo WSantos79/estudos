ce581b9f70c1d155ae643e50df9c314d17788179
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2022-05-11 21:48:02 -0300

    estudando

---
+++ JavaScript/Verificar CPF/script.js
@@ -0,0 +1,55 @@
+const cpf = '34532622808';
+
+validarCPF(cpf);
+
+function validarCPF(cpf){
+    var cpfFormatado = cpf.replace(/\./g, '').replace(/\-/g, '');
+    
+    let primeiroDigito = firstD(cpfFormatado);
+    let segundoDigito = secondD(cpfFormatado, primeiroDigito);
+
+    var digitoVerificador = `${primeiroDigito}${segundoDigito}`;
+    var digitoAtual = `${cpfFormatado[9]}${cpfFormatado[10]}`
+    
+    if(digitoVerificador === digitoAtual){
+        console.log('CPF Verdadeiro');
+        console.log(`Os dois últimos digitos são ${digitoVerificador}`);
+    }else{
+        console.log('CPF Falso');
+        console.log(`Os dois últimos digitos deveriam ser ${digitoVerificador}`);
+    }
+}
+
+function firstD(cpf){
+    let primeiroDigito = 0;
+    x = 0;
+    for(i = 10; i != 1; i--){        
+        primeiroDigito = (cpf[x] * i) + primeiroDigito;       
+        x++;
+    }
+    primeiroDigito = primeiroDigito % 11;
+    if(primeiroDigito === 0 || primeiroDigito === 1){
+        return 0;
+    }else{
+        return 11 - primeiroDigito;
+    }
+}
+
+function secondD(cpf, primeiroDigito){
+    let segundoDigito = 0;
+    x = 0;
+    for(i = 11; i != 1; i--){
+        if(cpf[x]){
+            segundoDigito = (cpf[x] * i) + segundoDigito;
+        }else{
+            segundoDigito = (primeiroDigito * i) + segundoDigito;
+        }
+        x++;
+    }
+    segundoDigito = segundoDigito % 11;
+    if(segundoDigito === 0 || segundoDigito === 1){
+        return 0;
+    }else{
+        return 11 - segundoDigito;
+    }
+}
