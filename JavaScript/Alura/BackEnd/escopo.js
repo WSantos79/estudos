ef9610c550d5eb2d87e757de9460cd99309588bd
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2024-11-25 21:43:04 -0300

    commit

---
+++ JavaScript/Alura/BackEnd/escopo.js
@@ -0,0 +1,9 @@
+if (1 > 0) {
+    var estudante = 'carolina';
+    console.log(estudante)
+}
+
+console.log(estudante)
+
+let idade = 25;
+console.log('A idade é:', idade);
---
+++ JavaScript/Alura/BackEnd/teste.js
@@ -0,0 +1,10 @@
+const p1 = "a";
+const p2 = 2;
+
+const om = p1 * p2;
+
+console.log(p1 + p2);
+console.log(om)
+
+let numHex = 0X7C4; // Representa o número 1988 em hexadecimal --- sempre começar com 0X para representar hexadecimal
+console.log(numHex); // Saída: 10
